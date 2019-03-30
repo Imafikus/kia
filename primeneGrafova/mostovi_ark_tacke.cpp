b88924f55febb1045be29ea205208e32049b0a2d
Imafikus <aleksatesicteske@gmail.com>
2019-03-30 18:24:56 +0100

    Add buggy bridge detection

--- primeneGrafova/mostovi_ark_tacke.cpp
+++ primeneGrafova/mostovi_ark_tacke.cpp
@@ -29,60 +29,61 @@ public:
             listaPovezanosti[v].push_back(u);
     }
 
-    void dfs(int pocetniCvor)
+    void dfs(int cvor)
     {
-        posetiCvor(pocetniCvor);
-        cout << pocetniCvor << " ";
-        for(int sused : listaPovezanosti[pocetniCvor])
+        posetiCvor(cvor);
+        cout << cvor << " ";
+        for(int sused : listaPovezanosti[cvor])
         {
             if(!vecPosecen(sused))
                 dfs(sused);
         }
     }
 
-    void nadjiArtikTacke(int pocetniCvor)
+    void nadjiArtikTacke(int cvor)
     {
-        posetiCvor(pocetniCvor);
+        posetiCvor(cvor);
 
-        vremePosecivanja[pocetniCvor] = vreme;
-        najranijePosecenSused[pocetniCvor] = vreme;
+        vremePosecivanja[cvor] = vreme;
+        najranijePosecenSused[cvor] = vreme;
 
         vreme++;
 
         int decaTrenutnogCvora = 0;
 
-        auto begin = listaPovezanosti[pocetniCvor].begin();
-        auto end = listaPovezanosti[pocetniCvor].end();
+        auto begin = listaPovezanosti[cvor].begin();
+        auto end = listaPovezanosti[cvor].end();
 
         while(begin != end)
         {
             if(!vecPosecen(*begin))
             {
-                roditeljCvora[*begin] = pocetniCvor;
+                roditeljCvora[*begin] = cvor;
                 nadjiArtikTacke(*begin);
 
                 decaTrenutnogCvora++;
 
-                if(najranijePosecenSused[*begin] < najranijePosecenSused[pocetniCvor])
-                    najranijePosecenSused[pocetniCvor] = najranijePosecenSused[*begin];
+                if(najranijePosecenSused[*begin] < najranijePosecenSused[cvor])
+                    najranijePosecenSused[cvor] = najranijePosecenSused[*begin];
 
                 //? Proveri da li je koreni cvor, i ako jeste, da li je artik tacka
-                if(roditeljCvora[pocetniCvor] == -1 && decaTrenutnogCvora > 1)
-                    artikTacke.push_back(pocetniCvor);
+                if(roditeljCvora[cvor] == -1 && decaTrenutnogCvora > 1)
+                    artikTacke.push_back(cvor);
                 
                 //? ovo se desava kada cvor nije koren, gledamo pocetni cvor i njegove susede, ne postoji sused takav
                 //? da on za svog suseda ima neki cvor koji se pojavio pre pocetnog, onda ce pocetni cvor biti artikulaciona tacka
-                if(roditeljCvora[pocetniCvor] != -1 && vremePosecivanja[pocetniCvor] <= najranijePosecenSused[*begin])
-                    artikTacke.push_back(pocetniCvor);
+                if(roditeljCvora[cvor] != -1 && vremePosecivanja[cvor] <= najranijePosecenSused[*begin])
+                    artikTacke.push_back(cvor);
                 
             }
             //? proveravamo da li sused trenutnog cvora ima mozda neki cvor
             //? koji je ranije posecen a do kog moze da dodje, ako ima, to znaci
             //? i da nas trenutni cvor moze da dodje do njega pa radimo update
-            else if(*begin != roditeljCvora[pocetniCvor])
+            //? pitamo da li je taj sused razlicit od roditelja ovog cvora jer ne zelimo da idemo na tu stranu
+            else if(*begin != roditeljCvora[cvor])
             {
-                if(najranijePosecenSused[pocetniCvor] >= najranijePosecenSused[*begin])
-                    najranijePosecenSused[pocetniCvor] = najranijePosecenSused[*begin];
+                if(najranijePosecenSused[*begin] < najranijePosecenSused[cvor])
+                    najranijePosecenSused[cvor] = najranijePosecenSused[*begin];
             }
 
             begin++;
@@ -98,6 +99,61 @@ public:
         {
             cout << i << endl;
         }
+    }
+
+    void nadjiMostove(int cvor)
+    {
+        posetiCvor(cvor);
+
+        vremePosecivanja[cvor] = vreme;
+        najranijePosecenSused[cvor] = vreme;
+
+        vreme++;
+
+        int decaTrenutnogCvora = 0;
+
+        auto begin = listaPovezanosti[cvor].begin();
+        auto end = listaPovezanosti[cvor].end();
+
+        while(begin != end)
+        {
+            if(!vecPosecen(*begin))
+            {
+                roditeljCvora[*begin] = cvor;
+                nadjiMostove(*begin);
+
+                decaTrenutnogCvora++;
+
+                if(najranijePosecenSused[*begin] < najranijePosecenSused[cvor])
+                    najranijePosecenSused[cvor] = najranijePosecenSused[*begin];
+
+                //? Proveravamo da li je trenutni cvor najraniji posecen od svih svojih suseda, ako jeste, onda je ova grana most
+                if(vremePosecivanja[cvor] < najranijePosecenSused[*begin])
+                    cout << cvor << "->" << *begin << endl;
+            }
+            
+            else if(*begin != roditeljCvora[cvor])
+            {
+                if(najranijePosecenSused[cvor] >= najranijePosecenSused[*begin])
+                    najranijePosecenSused[cvor] = najranijePosecenSused[*begin];
+            }
+
+            begin++;
+        }
+    }
+    
+    void stampajVremePosecivanja()
+    {
+        for(int i : vremePosecivanja)
+            cout << i << " ";
+        cout << endl;
+    }
+
+    void stampajNajranije()
+    {
+        for(int i : najranijePosecenSused)
+            cout << i << " ";
+        cout << endl;
     }
 
 private:
@@ -128,8 +184,7 @@ private:
 
 int main()
 {
-    int brojCvorova = 6;
-    Graf graf(brojCvorova);
+    Graf graf(5);
 
     graf.dodajGranu(0, 2);
     graf.dodajGranu(0, 3);
@@ -139,5 +194,9 @@ int main()
 
     graf.nadjiArtikTacke(0);
     graf.ispisiArtikTacke();
+
+    graf.stampajVremePosecivanja();
+    graf.stampajNajranije();
+    graf.nadjiMostove(0);
 
 }
