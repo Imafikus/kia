32cc6d6af7fae9c099c76be7e1a0beb38899c022
Aleksa Tesic <tesic@getpenta.com>
2019-06-03 22:17:02 +0200

    Dodao bolji rabinKarp

--- obradaTeksta/rabinKarp.cpp
+++ obradaTeksta/rabinKarp.cpp
@@ -1,45 +1,63 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> rabinKarp(string uzorak, string tekst)
+vector<int> rabinKarp(string pattern, string tekst)
 {
     int P = 31;
     int M = 1e9;
 
-    int lenUzorak = uzorak.size();
+    int lenUzorak = pattern.size();
     int lenTekst = tekst.size();
 
-    vector<long long> stepenP(max(lenUzorak, lenTekst));
+    vector<long long> stepenP(lenTekst);
+
+    // stepenP[i] == P^i (mod M)
+
     stepenP[0] = 1;
     for (int i = 1; i < stepenP.size(); i++)
     {
+        // P^i = P^(i-1) * P
         stepenP[i] = (stepenP[i - 1] * P) % M;
     }
 
     //? Racunamo hash vrednosti svih prefiksa datog niza
+    //? Radi se inkremnetalno, refer to formula u donjoj for petlji
     vector<long long> hashTeksta(lenTekst + 1, 0);
     for (int i = 0; i < lenTekst; i++)
     {
-        hashTeksta[i + 1] = (hashTeksta[i] + (tekst[i] - 'a' + 1) * stepenP[i]) % M;
+        // hashTeksta[n] = Sum[c[i] * P^i, i = [0 .. n-1]
+        hashTeksta[i + 1] = (hashTeksta[i] + tekst[i] * stepenP[i]) % M;
     }
 
     long long hashUzorka = 0;
     for (int i = 0; i < lenUzorak; i++)
     {
-        hashUzorka = (hashUzorka + (uzorak[i] - 'a' + 1) * stepenP[i]) % M;
+        hashUzorka = (hashUzorka + pattern[i]  * stepenP[i]) % M;
     }
 
     vector<int> pojavljivanja;
     for (int i = 0; i <= (lenTekst - lenUzorak); i++)
     {
-        long long tekuciHash = (hashTeksta[i + lenUzorak] + M - hashTeksta[i]) % M;
+        // [5, 10] = [0, 10] - [0, 4], dodaje se M da ne odemo u negativan modul
+        long long tekuciHash = (hashTeksta[i + lenUzorak] - hashTeksta[i] + M) % M;
 
+        // tekuciHash = hash[i .. i+lenUzorak] = hash[0 .. i+lenUzorak] - hash[0 .. i]
+        //            = c[i] * P^i + c[i+1] * P^i+1 + ..... + c[i+lenUzorak-1] * P^(i+lenUzorak-1)
+
+        // hashUzorka = u[0] * P^0 + u[1]   * P^1   + ..... + u[lenUzorak-1]   * P^(lenUzorak-1)
+        // mnozimo sa P^i gornju formulu
         if (tekuciHash == (hashUzorka * stepenP[i]) % M)
         {
-            pojavljivanja.push_back(i);
+            // dodatna brute force provera da li su jednaki, ako je mozda doslo do promasaja
+            // u hesiranju
+            if (equal(tekst.begin() + i, tekst.begin() + i + lenUzorak, pattern.begin()))
+            {
+                pojavljivanja.push_back(i);
+            }
         }
     }
 
@@ -48,7 +66,7 @@ vector<int> rabinKarp(string uzorak, string tekst)
 
 int main()
 {
-    vector<int> pojavljivanja = rabinKarp("test", "testera_testerisati_testove");
+    vector<int> pojavljivanja = rabinKarp("TEST", "TdRT");
 
     for (int i : pojavljivanja)
     {
