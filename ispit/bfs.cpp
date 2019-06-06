382fd6f0d992baeb070bb8c777c8e2d3f428fb3b
Aleksa Tesic <tesic@getpenta.com>
2019-06-06 13:12:36 +0200

    Dodao bfs

--- ispit/bfs.cpp
+++ ispit/bfs.cpp
@@ -1,11 +1,10 @@
-#include <vector>
 #include <iostream>
-#include <stack>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 void stampajVektor(vector<T> v)
 {
     for(T e : v)
@@ -13,30 +12,30 @@ void stampajVektor(vector<T> v)
     cout << endl;
 }
 
-vector<int> dfs(vector<vector<int>> listaPovezanosti, vector<bool> posecen)
+vector<int> bfs(vector<vector<int>> listaPovezanosti, vector<bool> posecen)
 {
-    vector<int> dfsObilazak;
-    stack<int> s;
-    s.push(0);
+    vector<int> bfsObilazak;
+    
+    queue<int> neposeceni;
+    neposeceni.push(0);
     posecen[0] = true;
 
-    while(!s.empty())
+    while(!neposeceni.empty())
     {
-        int cvor = s.top();
-    
-        s.pop();
-        dfsObilazak.push_back(cvor);
+        int cvor = neposeceni.front();
+        bfsObilazak.push_back(cvor);
+        neposeceni.pop();
 
         for(int sused : listaPovezanosti[cvor])
         {
             if(!posecen[sused])
             {
-                s.push(sused);
+                neposeceni.push(sused);
                 posecen[sused] = true;
             }
         }
     }
-    return dfsObilazak;
+    return bfsObilazak;
 }
 
 int main()
@@ -44,8 +43,6 @@ int main()
     vector<vector<int>> listaPovezanosti = {{1, 3, 4}, {0, 2}, {1, 3}, {0, 2}, {0, 5}, {4}};
     vector<bool> posecen(listaPovezanosti.size(), false);
 
-    stampajVektor(dfs(listaPovezanosti, posecen));
-
-
+    stampajVektor(bfs(listaPovezanosti, posecen));
 
 }
